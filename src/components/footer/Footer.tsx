0cc609dafbcd7353a34e47b5b8beb46260c2b86a
jared james <jaredjames.personal@gmail.com>
2021-12-14 16:13:39 +1300

    footer component created with date and time

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import styles from './footer.module.scss'
 const Footer = () => {
-    return <div className={styles.container}></div>
+    return (
+        <div className={styles.container}>
+            {' '}
+            <p>Made by © Jared James {new Date().getFullYear()}</p>
+        </div>
+    )
 }
 
 export default Footer
