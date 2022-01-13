85195742fcae5e16415ca58ddf9f030bb48febb7
jared james <jaredjames.personal@gmail.com>
2022-01-14 10:14:30 +1300

    Header nav now links to corresponding points in website

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,3 +1,4 @@
+import { Link as LinkS } from 'react-scroll'
 import styles from './Header.module.scss'
 
 const Header = () => {
@@ -9,10 +10,19 @@ const Header = () => {
             <div className={styles.navigation__container}>
                 <nav>
                     <ul className={styles.list}>
-                        <li className={styles.list__item}>About</li>
-                        <li className={styles.list__item}>Mortgage</li>
-                        <li className={styles.list__item}>Savings</li>
-                        <li className={styles.list__item}>Results</li>
+                        <LinkS to="mortgage" smooth duration={350} spy>
+                            <li className={styles.list__item}>Mortgage</li>
+                        </LinkS>
+                        <LinkS to="expenses" smooth duration={500} spy>
+                            <li className={styles.list__item}>
+                                Income and Expenses
+                            </li>
+                        </LinkS>
+                        <LinkS to="deposit" smooth duration={500} spy>
+                            <li className={styles.list__item}>
+                                Deposit Timeline
+                            </li>
+                        </LinkS>
                     </ul>
                 </nav>
             </div>
