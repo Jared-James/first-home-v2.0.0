e6a6fd7b8c1be8e3c9584d7678a3f22baeba3bbd
jared james <jaredjames.personal@gmail.com>
2022-03-04 08:49:54 +1300

    removed image and started reworking front page

--- src/components/landing-page/LandingPage.tsx
+++ src/components/landing-page/LandingPage.tsx
@@ -5,65 +5,41 @@ import styles from './LandingPage.module.scss'
 const LandingPage = () => {
     return (
         <div className={styles.landingPage__container}>
-            <div className={styles.landingPage__image}>
+            {/* <div className={styles.landingPage__image}>
                 <div className={styles.landingPage__image_item}>
                     <img src="/house.jfif" alt="house in the woods" />
                 </div>
-            </div>
+            </div> */}
             <div className={styles.landingPage__steps}>
                 <div className={styles.landingPage__steps_item}>
                     <div className={styles.landingPage__title}>
                         <h2 className={styles.landingPage__title_text}>
-                            Mortgage Repayment
+                            Buying your first home
                         </h2>
                     </div>
                     <div className={styles.landingPage__Body}>
                         <p className={styles.landingPage__Body_text}>
-                            Using the mortgage calculator, we will take your
-                            deposit, the interest rate and the length of the
-                            loan to calculate your repayments.
+                            Information is key when looking to purchase your
+                            first home. We aim to provide you with the tools
+                            that will make your life just that little bit easier
+                        </p>
+                        <p className={styles.landingPage__Body_text}>
+                            First find out what your mortgage repayments will be
+                            based on the house price and your deposit
+                        </p>
+                        <p className={styles.landingPage__Body_text}>
+                            Then look at your income and expenses to determine
+                            how much money you can put towards paying off your
+                            mortgage
+                        </p>
+                        <p className={styles.landingPage__Body_text}>
+                            Finally, we can look at how long based on all the
+                            above it will take you to achieve your deposit
                         </p>
                         <LinkS to="mortgage" smooth duration={350} spy>
-                            <Button variant="outlined" type="submit">
+                            {/* <Button variant="outlined" type="submit">
                                 Continue
-                            </Button>
-                        </LinkS>
-                    </div>
-                </div>
-                <div className={styles.landingPage__steps_item}>
-                    <div className={styles.title}>
-                        <h2 className={styles.landingPage__title_text}>
-                            Income and Expenses
-                        </h2>
-                    </div>
-                    <div className={styles.landingPage__Body}>
-                        <p className={styles.landingPage__Body_text}>
-                            We then look your income to calculate how much money
-                            you have remaining after expenses.
-                        </p>
-                        <LinkS to="expenses" smooth duration={500} spy>
-                            <Button variant="outlined" type="submit">
-                                Continue
-                            </Button>
-                        </LinkS>
-                    </div>
-                </div>
-                <div className={styles.landingPage__steps_item}>
-                    <div className={styles.title}>
-                        <h2 className={styles.landingPage__title_text}>
-                            Deposit Timeline
-                        </h2>
-                    </div>
-                    <div className={styles.landingPage__Body}>
-                        <p className={styles.landingPage__Body_text}>
-                            Finally, based on a percentage of your savings and
-                            income, we can work out how long it will take to
-                            achieve your deposit.
-                        </p>
-                        <LinkS to="deposit" smooth duration={500} spy>
-                            <Button variant="outlined" type="submit">
-                                Continue
-                            </Button>
+                            </Button> */}
                         </LinkS>
                     </div>
                 </div>
@@ -71,5 +47,11 @@ const LandingPage = () => {
         </div>
     )
 }
+
+// <LinkS to="expenses" smooth duration={500} spy>
+// <Button variant="outlined" type="submit">
+//     Continue
+// </Button>
+// </LinkS>
 
 export default LandingPage
